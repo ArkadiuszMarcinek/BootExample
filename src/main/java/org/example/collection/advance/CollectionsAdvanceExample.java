f48ebe4d2d177a40b4b92295f93529bec4f7cb96
Arkadiusz Marcinek <arek2244@gmail.com>
2020-06-03 15:00:15 +0200

    pierwsze zadania, zadanie z segregacja da sie zrobic optymalniej

--- src/main/java/org/example/collection/advance/CollectionsAdvanceExample.java
+++ src/main/java/org/example/collection/advance/CollectionsAdvanceExample.java
@@ -1,14 +1,31 @@
 package org.example.collection.advance;
 
+import org.example.BootExampleUtils;
 import org.example.dto.Mall;
 import org.example.dto.Product;
 import org.example.dto.Store;
+import org.example.exception.BootExampleException;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionsAdvanceExample implements CollectionsAdvanceExampleInterface {
 
     @Override
     public Product getTheMostAvailableProductFromStore(Store store) {
-        return null;
+        Map<Product,Long> values = store.getProducts().stream()
+        .collect(Collectors.groupingBy(a -> a, Collectors.counting()));
+        if(values.isEmpty())
+            throw new BootExampleException(BootExampleUtils.NO_PRODUCT_FOUND);
+        return values.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .orElseThrow(new BootExampleException(BootExampleUtils.NO_PRODUCT_FOUND))
+                .map(Map.Entry::getKey).get();
+
+
     }
 
     @Override
--- src/main/java/org/example/collection/beginner/CollectionsBeginnerExample.java
+++ src/main/java/org/example/collection/beginner/CollectionsBeginnerExample.java
@@ -1,5 +1,8 @@
 package org.example.collection.beginner;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -58,22 +61,11 @@ public class CollectionsBeginnerExample implements CollectionBeginnerExampleInte
                 .sorted(Comparator.nullsLast(this::customSort))
                 .collect(Collectors.toList());
     }
-
-    @Override
-    public List<User> getUsersWithCorrectedAge(List<User> users) {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public List<Sex> getUsersWithCorrectedGender(List<User> users) {
-        return Collections.emptyList();
-    }
-
     private int customSort(User user1,User user2){
-         boolean pierwszyZnak = user1.getName().substring(0,1).equals(user2.getName().substring(0,1));
-         boolean znakOrazWiekszyWiek = pierwszyZnak &&user1.getAge()>user2.getAge();
-         boolean znakOrazMniejszyWiek = pierwszyZnak &&user1.getAge()<user2.getAge();
-         boolean znakOrazEqualsWiek = pierwszyZnak &&user1.getAge()==user2.getAge();
+        boolean pierwszyZnak = user1.getName().substring(0,1).equals(user2.getName().substring(0,1));
+        boolean znakOrazWiekszyWiek = pierwszyZnak &&user1.getAge()>user2.getAge();
+        boolean znakOrazMniejszyWiek = pierwszyZnak &&user1.getAge()<user2.getAge();
+        boolean znakOrazEqualsWiek = pierwszyZnak &&user1.getAge()==user2.getAge();
 
         if( znakOrazEqualsWiek )
             return (user1.getName().compareTo(user2.getName()));
@@ -84,4 +76,48 @@ public class CollectionsBeginnerExample implements CollectionBeginnerExampleInte
         else
             return 1;
     }
+
+    @Override
+    public List<User> getUsersWithCorrectedAge(List<User> users) {
+
+        return Optional.ofNullable(users)
+                .orElseGet(Collections::emptyList).stream()
+                .map(this::getAgeOfUser)
+                .collect(Collectors.toList());
+    }
+
+    private User getAgeOfUser(User user){
+        Optional.ofNullable(user).ifPresent(this::defineAgeOfUser);
+        return user;
+    }
+    private void defineAgeOfUser(User user){
+        if(LocalDate.now().isBefore(user.getBirthday()))
+            user.setAge(0);
+        else user.setAge( (int)ChronoUnit.YEARS.between(user.getBirthday(),LocalDate.now()));
+
+
+    }
+
+    @Override
+    public List<Sex> getUsersWithCorrectedGender(List<User> users) {
+        return Optional.ofNullable(users)
+                .orElseGet(Collections::emptyList).stream()
+                .map(this::getSexOfUser)
+                .collect(Collectors.toList());
+    }
+
+    private Sex getSexOfUser(User user)
+    {
+        return Optional.ofNullable(user).isPresent() ? defineSexOfUser(user) : Sex.U;
+    }
+    private Sex defineSexOfUser(User user)
+    {
+        final int lastCharOfName = user.getName().length()-1;
+        if(user.getName().endsWith("a"))
+            return Sex.F;
+        if(!Character.isLetter(user.getName().charAt(lastCharOfName)))
+            return Sex.U;
+        else return Sex.M;
+    }
+
 }
